f65c2eaf65a31199397c93e2fed1b67e53b545ae
gerardom41 <gerardomolina9134@gmail.com>
2024-02-04 20:46:55 -0500

    Update assignment2.py
    Assignment 2 work with csv download, process, and display

--- assignment2.py
+++ assignment2.py
@@ -3,20 +3,63 @@ import urllib.request
 import logging
 import datetime
 
+logging.basicConfig(filename="errors.log", level=logging.ERROR)
+
 def downloadData(url):
     """Downloads the data"""
-    pass
+    getData = urllib.request.urlopen(url)
+    csvData = getData.read().decode('utf-8')
+
+    return csvData
 
 def processData(file_content):
-    pass
+    idNameBday = {}
 
+    for line in file_content.split('\n'):
+        if line:
+            parts = line.split(',')
+            try:
+                personId, name, birthdayString = parts
+                birthday = datetime.datetime.strptime(birthdayString, "%d/%m/%Y").date()
+                idNameBday[int(personId)] = (name, birthday)
+
+            except Exception:
+                loggingError(personId)
+                continue
+
+    return idNameBday
+
+def loggingError(personId):
+    logging.error(f"Error processing line #{personId} for ID {personId}, where"\
+        f" #{personId} and {personId} are the line number and ID,respectively,"\
+        "of the person in the CSV file that has a mallformed date.")
 
 def displayPerson(id, personData):
-    pass
+    for personId, (name, birthday) in personData.items():
+        if id == personId:
+            print(f"Person #{personId} is {name} with a birthday of {birthday}, where {personId} is"\
+                f" the id the user requested,{name} is the name of the person from the file"\
+                f" and {birthday} is the birthday of the user")
+
+    if id not in personData:
+        print("No user found with that id")
 
 def main(url):
     print(f"Running main with URL = {url}...")
 
+    data = downloadData(url)
+    idNameBday = processData(data)
+
+    while data: 
+        x = int(input("Enter an Id number: "))
+
+        if x <= 0:
+            exit()
+        else:
+            displayPerson(x, idNameBday)
+
+    else:
+        print("No data found")
 
 if __name__ == "__main__":
     """Main entry point"""
